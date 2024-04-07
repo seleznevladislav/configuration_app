4bd32c783866675a66177ab4b0f639163226f3a7
Alexandr Zarubin <zarubin_14@list.ru>
2024-04-07 21:51:07 +0300

    add parametrization MPST008 TTOR

--- Qt/buildmathmodel.h
+++ Qt/buildmathmodel.h
@@ -200,7 +200,7 @@ namespace BuildMathModel {
 		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_005_kamera();
 		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_006_RezhetkaTeplTube();
 		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_007_FlanecKozhux();
-		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_008_FlanecSpecial();
+		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_008_FlanecSpecial(double ttDiam, double ttThickness);
 		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_009_curevedTube(double ttDiam, double ttThickness, double t);
 		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_010_Connector(double ktDiam, double ktThickness, double t);
 		static SPtr<MbSolid> ParametricModelCreator::Zarubincreate_011_ConnectorWithFlanec(double ktDiam, double ktThickness, double t, double visotaOpori);
